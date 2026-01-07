715c4d9eaf3408bf0cb6b791a8253e982362af3c
ramu siripalli <ramsiripalli2772@gmail.com>
2026-01-07 13:10:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,31 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const priceElements = document.querySelectorAll(".price");
+	let total = 0;
+
+	priceElements.forEach((price) => {
+		total += Number(price.textContent);
+	});
+
+	const table = document.querySelector("table");
+
+	const existingTotal = document.getElementById("total-row");
+	if(existingTotal){
+		existingTotal.remove();
+	}
+
+	const totalRow = document.createElement("tr");
+	totalRow.id = 'total-row';
+
+	const totalCell = document.createElement("td");
+	totalCell.colspan= 2;
+	totalCell.style.fontWeight = "bold";
+	totalCell.style.textAlign = "center";
+	totalCell.textContent = `Total Price = Rs ${total}`;
+
+	totalRow.appendChild(totalCell);
+	table.appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
