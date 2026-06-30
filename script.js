b7b87e93fc36fbcc157219d68ed9e715531d2ecd
ramu siripalli <ramsiripalli2772@gmail.com>
2026-06-30 14:01:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
+const getSumBtn = document.createElement("button");
+getSumBtn.textContent = "Get Total Price";
+
+document.body.appendChild(getSumBtn);
+
 const getSum = () => {
+//Add your code here
+	const prices = document.querySelectorAll(".prices");
+	let total= 0;
+	prices.forEach(function(price){
+total+= Number(price.textContent);
+});
 
-    const prices = document.querySelectorAll(".prices");
-
-    let total = 0;
-
-    prices.forEach((price) => {
-        total += Number(price.textContent);
-    });
-
-    // Remove previous total row if it exists
-    const oldAns = document.getElementById("ans");
+	const oldAns = document.getElementById("ans");
     if (oldAns) {
         oldAns.parentElement.remove();
     }
 
-    const row = document.createElement("tr");
 
-    const cell = document.createElement("td");
-    cell.id = "ans";          // IMPORTANT
-    cell.colSpan = 2;
-    cell.textContent = total;
-
-    row.appendChild(cell);
-
-    document.querySelector("table").appendChild(row);
+	const row = document.createElement("tr");
+	const cell = document.createElement("td");
+	cell.id="ans";
+	cell.colSpan=2;
+	cell.textContent = total;
+	row.appendChild(cell);
+	
+  document.querySelector("table").appendChild(row);
 };
+
+getSumBtn.addEventListener("click", getSum);
+
